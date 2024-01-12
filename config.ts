9f10390484833fbc6ae84b3661c65fe734f1248e
Martin George <marty@dingo.foo>
2024-01-12 13:04:15 +0000

    commit

--- config.ts
+++ config.ts
@@ -18,7 +18,22 @@ const blogCollection = defineCollection({
   }),
 });
 
-const portfolioCollection = defineCollection({})
+const portfolioCollection = defineCollection({
+  schema: ({ image }: any) => z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    // author: z.object({
+    //   name: z.string(),
+    //   avatar: z.string(),
+    // }),
+    // date: z.date().nullable(),
+    description: z.string().nullable(),
+    // tags: z.array(z.string()).nullable(),
+    preview_image: image().optional(),
+    // series: z.string().optional(),
+  }),
+  
+})
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
