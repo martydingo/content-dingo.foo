d5a89fc64e6073e04af6ee306a385d580e1a3ed9
Martin George <marty@dingo.foo>
2024-01-12 11:55:25 +0000

    commit

--- config.ts
+++ config.ts
@@ -18,8 +18,10 @@ const blogCollection = defineCollection({
   }),
 });
 
+const portfolioCollection = defineCollection({})
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
+  'portfolio': portfolioCollection
 };
